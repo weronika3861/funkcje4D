e0739eee06c9589550d48b167062fa642110f454
hubabuuba <65412002+hubabuuba@users.noreply.github.com>
2020-05-15 18:33:21 +0200

    Add files via upload

---
+++ txt_generator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.8
+
+from numpy import arange
+
+
+def fun1(x, y, z):
+    return x**2 + y**2 + z**2
+
+
+def fun2(x, y, z):
+    return x
+
+
+# other functions...
+
+
+# can be altered:
+lim_down = -5.0
+lim_up = 5.0
+step = 0.5
+fun = fun2
+
+
+gen_range = arange(lim_down, lim_up + step, step)
+
+with open('data.txt', 'w') as file:
+    for x in gen_range:
+        for y in gen_range:
+            for z in gen_range:
+                fun_val = fun(x, y, z)
+                file.write(f'{x} {y} {z} {fun_val}\n')

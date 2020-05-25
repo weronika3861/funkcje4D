b3bfe93726bd42f3d778f279df3481e8d12f4ebf
hubabuuba <nataliaszczerba00@gmail.com>
2020-05-25 12:40:28 +0200

    Przycisk "Load File" laduje plik txt i przekazuje jego zawartosc do vectora(Point4D)

--- GUIMyFrame1.cpp
+++ GUIMyFrame1.cpp
@@ -1,4 +1,17 @@
 #include "GUIMyFrame1.h"
+#include <fstream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+class Point4D
+{
+public:
+	Point4D(double x, double y, double z, double f) : x(x), y(y), z(z), f(f) {}
+private:
+	double x, y, z, f;
+};
+
+std::vector<Point4D> data;
 
 GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
 :
@@ -10,6 +23,23 @@ MyFrame1( parent )
 void GUIMyFrame1::m_loadOnButtonClick( wxCommandEvent& event )
 {
 // TODO: Implement m_loadOnButtonClick
+	wxFileDialog WxOpenFileDialog(this, wxT("Choose a file"), wxT(""), wxT(""), wxT("Text file (*.txt)|*.txt"), wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+
+	if (WxOpenFileDialog.ShowModal() == wxID_OK)
+	{
+		std::ifstream in(WxOpenFileDialog.GetPath().ToAscii());
+		if (in.is_open())
+		{
+			data.clear();
+			while (!in.eof())
+			{
+				double x, y, z, f;
+				in >> x >> y >> z >> f;
+				data.push_back(Point4D(x, y, z, f));
+			}
+			in.close();
+		}
+	}
 }
 
 void GUIMyFrame1::m_saveOnButtonClick( wxCommandEvent& event )

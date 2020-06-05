7333ee402323e89a4050d11512e2025bfa6db0bb
hubabuuba <nataliaszczerba00@gmail.com>
2020-06-05 22:23:23 +0200

    Nie wywala bledu dla przyciskow 4-6 przy duzej ilosci pktow

--- GUIMyFrame1.h
+++ GUIMyFrame1.h
@@ -49,7 +49,6 @@ public:
 	std::vector<double> XData, YData, ZData, FData; //wektory przechowujace wartosci x, y, z, f (z pliku)
 	std::vector <double>* XAxisArg, *YAxisArg, *ZAxisArg;
 	std::vector <std::array<double, 3>> AxisArgs;
-	std::vector <double> Xnew, Ynew, Znew; //wektory pomocnicze przy przyciskach 4,5,6
 	double FunMax, FunMin; //max i min aktualnej fukcji 4D (czyli wektora FData)
 	double PointRange; //zakres argumentow x, y, z (max - min)
 	int SliceNumber; //liczba na slajderze, czyli aktualny numer przekroju (mo¿e byæ 100 przekrojów)
